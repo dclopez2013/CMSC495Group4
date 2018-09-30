00f5aa68218ba6ee0e79385cf12bcaeffc5165ee
freecodecampC9 <39604355+freecodecampC9@users.noreply.github.com>
2018-09-30 14:25:57 -0500

    db Connect with additional todo list, and variable modifications
    changed scope of variables and acesses.
    added comments and descriptions.
    added todo notes for db and sql queries.
    created methods for clearing temp data fields after being used to pass variables to and from methods and database.

--- src/dbConnect.java
+++ src/dbConnect.java
@@ -6,54 +6,78 @@ Date : 27 Sep 18
 Purpose : this class provides connection to the database and queries
 the database
  */
+
 package capstone;
+
+//IMPORTS 
 import java.sql.*;
 import java.math.*;
-/**
- *
- * @author dclop
- */
+//END IMPORTS
+
+//DATABASE CONNECT CLASS
 public class dbConnect {
     
+
+    //BEGIN VARIABLES
     //database info
-    String URL = "jdbc:oracle::1521:";
-    String USER = "username";
-    String PASS = "password";
+    private String URL = "jdbc:oracle::1521:";
+    private String USER = "username";
+    private String PASS = "password";
     private boolean isConnected = false;
     private static Connection con;
             
     //transaction holder variables
-    String tempUid = null;
-    String tempAccType = null;
-    float tempAmmount = null; 
-    float tempBalance= null;
+    private String tempUid = "";
+    private String tempAccType = "";
+    private float tempAmmount = 0.0; 
+    private float tempBalance= 0.0;
+
+
+    //ERROR Strings
+    private static final String errorUID = "UID NOT FOUND";
+
+
+    //END VARIABLES
+
+
+    //BEGIND METHODS
+
 
      //dynamically load the driver's class file into memory, 
      //which automatically registers it. This method is preferable 
      //because it allows you to make the driver registration configurable and portable.     
     
-    public void InitDB() throws SQLException{
+    protected void InitDB() throws SQLException{
     try{
-         Class.forName("org.apache.derby.jdbc.ClientDriver");
+         Class.forName("org.apache.derby.jdbc.ClientDriver"); //TODO : MATCH DRIVER WITH NEEDED DB TYPE
       } catch(ClassNotFoundException e) {
          System.out.println("Class not found "+ e);
       }
-    //connect to DB
+
+    //CONNECT TO DB WITH CREDENTIALS
     //TODO : insert correct ddb name and path with parameters
     con = DriverManager.getConnection(URL, USER, PASS);
     }
     
-    private boolean isClose() throws SQLException{
+    //check connection method
+    protected boolean isClose() throws SQLException{
         return con.isClosed();
     }
+
+    //DISCONNECT FROM DB
+    protected void disconnectDB() throws SQLException{
+        con.close();
+     }
     
-    private void withdraw(String uid,float ammount, String accType) throws SQLException{
+
+     //WITHDRAW METHOD
+    protected void withdraw(String uid,float ammount, String accType) throws SQLException{
        
         tempUid = uid;
         tempAmmount = ammount;
         tempAccType = accType;
         //deposit using prepared statement
-        PreparedStatment stmt = con.prepareStatment("select ? FROM Accounts WEHRE UID =?");
+        PreparedStatment stmt = con.prepareStatment("select ? FROM Accounts WEHRE UID =?");//TODO : MATCH SQL WITH ACTUAL DB
         stmt.setString(1, accType);
         stmt.setString(2, uid)
         ResultSet resultSet = stmt.executeQuery();
@@ -64,7 +88,7 @@ public class dbConnect {
                 if(checkBalance()>=tempBalance){
                     tempBalance -= ammount;
 
-                    PreparedStatment stmtUpdate = con.prepareStatment("UPDATE Accounts SET ? = ? WHERE UID =?");
+                    PreparedStatment stmtUpdate = con.prepareStatment("UPDATE Accounts SET ? = ? WHERE UID =?");//TODO : MATCH SQL WITH ACTUAL DB
                     stmtUpdate.setString(1, accType);
                     stmtUpdate.setFloat(2, tempBalance);
                     stmtUpdate.setString(3,tempUid);
@@ -84,30 +108,31 @@ public class dbConnect {
         stmt.closeOnCompletion();*/
     }
     
-    private void desposit(String uid,float ammount, String accType) throws SQLException{   
+    protected void desposit(String uid,float ammount, String accType) throws SQLException{   
         tempUid = uid;
         tempAmmount = ammount;
         tempAccType = accType;
+
         //deposit using prepared statement
-        PreparedStatment stmt = con.prepareStatment("select ? FROM Accounts WEHRE UID =?");
+        PreparedStatment stmt = con.prepareStatment("select ? FROM Accounts WEHRE UID =?"); //TODO : MATCH SQL WITH ACTUAL DB
+        //INSERTS DATA 
         stmt.setString(1, accType);
+
         stmt.setString(2, uid)
         ResultSet resultSet = stmt.executeQuery();
         if(resultSet.next()){
         tempBalance = resultSet.getFloat(tempAccType.toString());
         tempBalance += ammount;
 
-        PreparedStatment stmtUpdate = con.prepareStatment("UPDATE Accounts SET ? = ? WHERE UID =?");
+        PreparedStatment stmtUpdate = con.prepareStatment("UPDATE Accounts SET ? = ? WHERE UID =?"); //TODO : MATCH SQL WITH ACTUAL DB
         stmtUpdate.setString(1, accType);
         stmtUpdate.setFloat(2, tempBalance);
         stmtUpdate.setString(3,tempUid);
-        /*
-        "UPDATE Registration " +
-        "SET age = 30 WHERE id in (100, 101)";
-        */
-            stmt.closeOnCompletion();
-            stmtUpdate.closeOnCompletion();
 
+        stmt.closeOnCompletion();
+        stmtUpdate.closeOnCompletion();
+        
+        this.clearAll();
         }
         else{
            
@@ -120,10 +145,10 @@ public class dbConnect {
          */
     }
     
-    private String getUid(String uid) throws SQLException{
+    protected String getUid(String uid) throws SQLException{
          //TODO UID retrieval logic
         tempUid = uid;
-        PreparedStatment stmt = con.prepareStatment("select count (uid) FROM Accounts WEHRE UID =?");
+        PreparedStatment stmt = con.prepareStatment("select count (uid) FROM Accounts WEHRE UID =?"); //TODO : MATCH SQL WITH ACTUAL DB
         stmt.setString(1, tempUid);
         ResultSet resultSet = stmt.executeQuery(); 
        
@@ -131,11 +156,13 @@ public class dbConnect {
         return tempUid;
         }
         else{
-        return "Uid Not Found";
+        return errorUID;
         }
     }
     
-    private float checkBalance(String uid, String accType) throws SQLException{
+
+    //CHECKS BALANCE OF ONE ACCOUNT AT A TIME
+    protected float checkBalance(String uid, String accType) throws SQLException{
        /* Statement stmt = con.createStatement();
          ResultSet rs = stmt.executeQuery ("");
          stmt.closeOnCompletion();
@@ -145,7 +172,7 @@ public class dbConnect {
         tempUid = uid;
         tempAccType = accType;
         //deposit using prepared statement
-        PreparedStatment stmt = con.prepareStatment("select ? FROM Accounts WEHRE UID =?");
+        PreparedStatment stmt = con.prepareStatment("select ? FROM Accounts WEHRE UID =?"); //TODO : MATCH SQL WITH ACTUAL DB
         stmt.setString(1, accType);
         stmt.setString(2, uid)
         ResultSet resultSet = stmt.executeQuery();
@@ -158,8 +185,37 @@ public class dbConnect {
     return tempBalance;
     }
     
-    private void disconnectDB() throws SQLException{
-       con.close();
+
+    
+    
+
+
+    //NULLIFY/CLEAR FIELDS
+    private void clearUid(){
+        this.tempUid ="";
     }
-      
+    private void clearAccType(){
+        this.tempAccType="";
+    }
+    private void clearAmmount(){
+        this.tempAmmount=0.00;
+    }
+    private void clearBalance(){
+        this.tempBalance=0.00;
+    }
+
+    private void clearAll(){
+        this.clearUid();
+        this.clearAccType();
+        this.clearAmmount();
+        this.clearBalance();
+    }
+
+    //END METHODS
 }
+//END CLASS
+
+/*
+TODO: verify SQL statements
+      CREATE DB With Tables and test data.
+*/ 
