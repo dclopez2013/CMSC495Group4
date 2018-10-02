4044b45b3a0e2d9d870dc2a68511673c6e64ddd8
sierrac <sierrac@33.33.33.1>
2018-10-01 20:37:51 -0400

    created account class and fix spacing in interest class

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,52 @@
+import java.sql.SQLException;
 
-public class Account {
-
+public class Account extends dbConnect {
+	
+	protected boolean checkAccountBalance(String uid, String accType) {
+		float accountBalance = 0;
+		
+		try {
+			accountBalance = checkBalance(uid, accType);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if (accountBalance > 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	protected boolean preformDeposit(String uid,float ammount, String accType) {
+		try {
+			desposit(uid, ammount, accType);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+	
+	protected void preformWithdraw(String uid,float ammount, String accType)  {
+		try {
+			withdraw(uid, ammount, accType);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return;
+	}
+	
+	protected float getAccountBalance(String uid, String accType) {
+		float accountBalance = 0;
+		
+		try {
+			accountBalance = checkBalance(uid, accType);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return accountBalance;
+	}
 }
