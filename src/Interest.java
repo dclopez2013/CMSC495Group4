71dbd8618660fd1db2b327bacd43354b9e8c3548
sierrac <sierrac@192.168.20.56>
2018-10-08 21:21:05 -0400

    changed date format

--- src/Interest.java
+++ src/Interest.java
@@ -4,21 +4,22 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 	public class Interest extends TimerTask {
 		
-		String transactionType = "interest";
-		String accountType = "savings";
 	    double accountBalance = 0;
 	    double fixedInterestRate = 0.02;
 	    double interest = 0;
 	    
 	  //This will be used to add date with transaction
-	    String date = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+	    Date myDate = new Date();
+	    String date = new SimpleDateFormat("MM/dd/yyyy").format(myDate);
 	    
 	    //Variables for DB
 	    private String URL = "jdbc:oracle::1521:";
@@ -37,15 +38,14 @@ import java.util.concurrent.TimeUnit;
 	    
 	    //This will calculate the interest (need a run method in a Class that extends TimerTask)
 	    public void run() {
-	    String accountBalanceQuery = "select ? FROM Accounts WHERE account_type =?";
+	    String accountBalanceQuery = "SELECT ? FROM Accounts";
 	    
 	    try {
 	    	PreparedStatement accountBalanceStmt = con.prepareStatement(accountBalanceQuery);
-	    	accountBalanceStmt.setString(1, "account_balance");
-	    	accountBalanceStmt.setString(2, accountType);
+	    	accountBalanceStmt.setString(1, "SavingAccount");
 	    	ResultSet resultSet = accountBalanceStmt.executeQuery();
 	    	if(resultSet.next()){
-	    		accountBalance = resultSet.getDouble("accountBalance");
+	    		accountBalance = resultSet.getDouble("SavingAccount");
 	    	}
 	    } catch(SQLException se) {
 	    	se.printStackTrace();
@@ -54,26 +54,12 @@ import java.util.concurrent.TimeUnit;
 	    interest = accountBalance * (fixedInterestRate/365);
 	    double newSavingsAccountBalance = accountBalance + interest;
 	    
-	    String insertTransactionQuery = "INSERT INTO Transaction Table (transaction_type, interest, account_type, date)" + " values (?, ?, ?, ?)";
-	    
-	    try {
-	    	PreparedStatement transactionStmt = con.prepareStatement(insertTransactionQuery);
-	    	transactionStmt.setString(1, transactionType);
-	    	transactionStmt.setDouble(2, interest);
-	    	transactionStmt.setString(3, accountType);
-	    	transactionStmt.setString(4, date);
-	    	
-	    	transactionStmt.execute();
-	    } catch(SQLException se) {
-	    	se.printStackTrace();
-	    }
-	    
-	    String updateSavingsAccountTotal = "UPDATE Account Table SET account_balance = ? WHERE account_type = ?";
+	    String updateSavingsAccountTotal = "UPDATE Accounts SET SavingAccount = ? AND Date = ?";
 	    
 	    try {
 	    	PreparedStatement accountUpdateStmt = con.prepareStatement(updateSavingsAccountTotal);
 	    	accountUpdateStmt.setDouble(1, newSavingsAccountBalance);
-	    	accountUpdateStmt.setString(2,  accountType);
+	    	accountUpdateStmt.setString(2, date);
 	    	
 	    	accountUpdateStmt.execute();
 	    } catch(SQLException se) {
@@ -83,6 +69,7 @@ import java.util.concurrent.TimeUnit;
 	   }
 	    
 	    public static void main(String[] args) {
+	    	//String date = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
 	    	Calendar today = Calendar.getInstance();
 	    	today.set(Calendar.HOUR_OF_DAY, 1);
 	    	today.set(Calendar.MINUTE, 0);
@@ -91,6 +78,7 @@ import java.util.concurrent.TimeUnit;
 	    	// This will run the task every morning at 1am
 	    	Timer timer = new Timer();
 	    	timer.schedule(new Interest(), today.getTime(), TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS)); 
+	 	    
 	    }
 
 	}
